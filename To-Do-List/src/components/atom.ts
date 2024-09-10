8eb75473a1116b16cc93407374302508bbd434da
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-09-11 00:49:42 +0900

    Recoil Selector 추가

--- To-Do-List/src/components/atom.ts
+++ To-Do-List/src/components/atom.ts
@@ -1,4 +1,5 @@
-import { atom } from "recoil";
+//atom: 단순 배열, selector: atom의 output을 변형시킴
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -6,7 +7,21 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
