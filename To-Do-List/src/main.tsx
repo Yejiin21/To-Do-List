5bf65086d07a9412042f663da564c5c927013111
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-09-10 14:22:10 +0900

    초기 세팅

---
+++ To-Do-List/src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient();
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
+
+export default App;
---
+++ To-Do-List/src/style/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#322c2c",
+  boxColor: "#484141",
+  textColor: "#f5f6fa",
+  accentColor: "#4b69fa",
+};
