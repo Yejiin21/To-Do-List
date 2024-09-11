d30d7bdd10a93733a926a2fda137c3ef58b79c53
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-09-11 17:09:03 +0900

    publishing

--- To-Do-List/src/App.tsx
+++ To-Do-List/src/App.tsx
@@ -1,14 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 import ToDoList from "./components/ToDoList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle``;
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
-      <ToDoList />
-    </>
+      <Routes>
+        <Route path="/" element={<ToDoList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
